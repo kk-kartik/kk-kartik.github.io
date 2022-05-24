fe2eacb40e06c761a4e76fd61aee3ad6cedc2d05
Kartik Kurupaswamy <98091442+kk-kartik@users.noreply.github.com>
2022-05-24 15:39:12 +0530

    Add files via upload
    Initial Commit

---
+++ javascript/index.js
@@ -0,0 +1,34 @@
+var typed=new Typed(".typ",{
+    strings: ["Kartik Kurupaswamy"],
+    typeSpeed:150,
+    backSpeed:10,
+    loop:false,
+});
+
+var fullImg=document.getElementById("fullimg");
+var img=document.getElementById("Img");
+function openImg(pic){
+    fullImg.style.display="flex";
+    Img.src=pic;
+}
+function closeImg(){
+    fullImg.style.display="none";
+}
+
+function sendEmail(){
+    Email.send({
+        SecureToken : "b143e160-fac4-4245-b8af-e100d7abc95f",
+        To : 'contact.kk.web@gmail.com',
+        From :  'contact.kk.web@gmail.com',
+        Subject : "New Contact Form - Website",
+        Body : "Name: " + document.getElementById("name").value + 
+                "<br> Email: "+ document.getElementById("email").value +
+                "<br> Phone No.: "+document.getElementById("phone").value +
+                "<br> Content: "+document.getElementById("msg").value
+    }).then(
+      message => alert("Thank you, your message was sent successfully.")
+    );
+}
+
+
+
